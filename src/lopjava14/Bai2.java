b7b5bc22813437adc166f58485ce032079348ed0
root <luann4099@gmail.com>
2020-07-29 20:03:56 +0700

    update code buổi 2

--- src/lopjava14/Bai2.java
+++ src/lopjava14/Bai2.java
@@ -20,6 +20,7 @@ public class Bai2 {
             choice = scanner.nextInt();
             switch(choice){
                 case 1 :
+                    System.out.println("Vui long nhập vào một số nguyên");
                     Scanner scanner1 = new Scanner(System.in);
                     int num = scanner1.nextInt();
                     if(soNT(num)==true){
@@ -30,15 +31,38 @@ public class Bai2 {
                     break;
                     
                 case 2:
+                     System.out.println("Vui long nhập vào một số nguyên");
                     Scanner scanner2 = new Scanner(System.in);
                     int num2 = scanner2.nextInt();
-                    soHoanHao(num2);
+                    if(soHoanHao(num2)==true){
+                        System.out.println("Đây là số hoàn hảo");
+                    }else{
+                        System.out.println("Đây ko phải là số hoàn hảo");
+                    }
+                    
                     break;
                     
                 case 3:
+                     System.out.println("Vui long nhập vào một số nguyên");
+                    Scanner scanner3 = new Scanner(System.in);
+                    int num3 = scanner3.nextInt();
+                    if(num3%2==0){
+                        System.out.println("Là số chẵn");
+                    }else{
+                        System.out.println("Là số lẻ");
+                    }
                     break;
                     
                 case 4:
+                     System.out.println("Vui long nhập vào một số nguyên");
+                    Scanner scanner4 = new Scanner(System.in);
+                    int num4 = scanner4.nextInt();
+                    int giaiThua = 1;
+                    for(int i=1; i<=num4;i++){
+                       giaiThua*=i;
+                    }
+                    System.out.println("Giai thua cua "+num4+" là:"+giaiThua);
+                    
                     break;
                     
                 case 5:
